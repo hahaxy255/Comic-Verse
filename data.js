c7ea4bc9e272319b36cf61d46ead609c9e29a53c
hahaxy255 <hahaxy255@gmail.com>
2025-07-11 14:42:07 +0700

    Update data.js

--- data.js
+++ data.js
@@ -525,33 +525,3 @@ const comics = [
   },
   // Tambahkan komik lain di sini dengan pola yang sama...
 ];
-
-
-// Sample genres
-        const genres = [
-            { id: 1, name: "Action", icon: "fas fa-person-running" },
-            { id: 2, name: "Adult", icon: "fas fa-user-shield" },
-            { id: 3, name: "Adventure", icon: "fas fa-mountain" },
-            { id: 4, name: "Comedy", icon: "fas fa-laugh" },
-            { id: 5, name: "Drama", icon: "fas fa-theater-masks" },
-            { id: 6, name: "Ecchi", icon: "fas fa-hot-tub" },
-            { id: 7, name: "Fantasy", icon: "fas fa-dragon" },
-            { id: 8, name: "Harem", icon: "fas fa-users" },
-            { id: 9, name: "Historical", icon: "fas fa-landmark" },
-            { id: 10, name: "Horror", icon: "fas fa-ghost" },
-            { id: 11, name: "Magic", icon: "fas fa-hat-wizard" },
-            { id: 12, name: "Martial Arts", icon: "fas fa-hand-fist" },
-            { id: 13, name: "Music", icon: "fas fa-music" },
-            { id: 14, name: "Mecha", icon: "fas fa-robot" },
-            { id: 15, name: "Mystery", icon: "fas fa-search" },
-            { id: 16, name: "Psychological", icon: "fas fa-brain" },
-            { id: 17, name: "Romance", icon: "fas fa-heart" },
-            { id: 18, name: "School", icon: "fas fa-school" },
-            { id: 19, name: "Sci-Fi", icon: "fas fa-flask" },
-            { id: 20, name: "Slice of Life", icon: "fas fa-mug-hot" },
-            { id: 21, name: "Superhero", icon: "fas fa-mask" },
-            { id: 22, name: "Supernatural", icon: "fas fa-moon" },
-            { id: 23, name: "Sports", icon: "fas fa-basketball-ball" },
-            { id: 24, name: "Thriller", icon: "fas fa-person-falling-burst" }
-        ];
-    
